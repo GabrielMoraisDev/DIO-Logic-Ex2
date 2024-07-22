46f9f0839ac6304f24a8f847e7940dc486b2b817
GabrielMoraisDev <leiteetec@gmail.com>
2024-07-22 09:18:57 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,26 @@
+function Resultados(vitorias, derrotas){
+    var resultado = vitorias - derrotas
+    return resultado
+}
+
+var nivel = 'Ferro'
+
+var saldoVitorias = Resultados(80, 40)
+
+if (saldoVitorias < 10){
+    nivel = 'Ferro'
+}else if (saldoVitorias >= 11 && saldoVitorias <=20){
+    nivel = 'Bronze'
+}else if (saldoVitorias >= 21 && saldoVitorias <=50){
+    nivel = 'Prata'
+}else if (saldoVitorias >= 51 && saldoVitorias <=80){
+    nivel = 'Ouro'
+}else if (saldoVitorias >= 81 && saldoVitorias <=90){
+    nivel = 'Diamante'
+}else if (saldoVitorias >= 91 && saldoVitorias <=100){
+    nivel = 'Lendário'
+}else if (saldoVitorias >= 101){
+    nivel = 'Imortal'
+}
+
+console.log("O Herói tem de saldo de ", saldoVitorias, "está no nível de ", nivel)
